15976ba4cefcfedea6c1eb55ffc7f51e4b032318
GLZ6 <rajmani.arya@tesco.com>
2018-04-04 12:13:01 +0530

    added support for chrome and opera

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Not published yet to test in firefox browser
 1. Open Firefox and open this link `about:debugging`.
 2. `Load Temperory addon` and select `manifest.json` from code repository.
 
-
+### On Opera or Chrome
+1. open Chrome or Opera and open this link `chrome://extensions`
+2. `Load Unpacked Extensions` and select `this repository dir` from code repo.
--- background.js
+++ background.js
@@ -1,7 +1,37 @@
-function openPage() {
-  browser.tabs.create({
-    url: browser.extension.getURL("index.html")
-  });
+// Opera 8.0+
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+// Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
+// Safari 3.0+ "[object HTMLElementConstructor]" 
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+// Edge 20+
+var isEdge = !isIE && !!window.StyleMedia;
+// Chrome 1+
+var isChrome = !!window.chrome && !!window.chrome.webstore;
+// Blink engine detection
+var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+if (isFirefox) {
+	function openPage() {
+		browser.tabs.create({
+			url: browser.extension.getURL("index.html")
+		});
+	}
+	
+	browser.browserAction.onClicked.addListener(openPage);
 }
 
-browser.browserAction.onClicked.addListener(openPage);
+if (isOpera || isChrome) {
+	chrome.browserAction.onClicked.addListener(function() {
+		chrome.tabs.query({
+			currentWindow: true,
+			active: true
+		}, function(tab) {
+			chrome.tabs.create({
+				"url": chrome.extension.getURL("index.html")
+			});
+		});
+	});
+}
